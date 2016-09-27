8c9f25536ef61e35269f5cd10e1d6c16e76b054f
danaver <averdanica@gmail.com>
2016-09-27 14:14:39 +0800

    added glyphicon

---
+++ home.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+ <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+	<button class="btn btn-danger"> 
+		<span class='glyphicon glyphicon-user'></span>
+	</button>
+</body>
+</html>
