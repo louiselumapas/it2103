ceab78ecc51967bd84600389221e6c6ce7058e66
Student Student <student@ceaccs.net>
2016-09-27 14:23:26 +0800

    added remove glyphicon

---
+++ home.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+	<button class="btn btn-danger">
+		<i class = 'glyphicon glyphicon-remove'></i>
+	</button>
+</body>
+</html>
